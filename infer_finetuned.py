c47545419b200bbffd6471f2c10fc54c3cb35363
RiyadHassen <riyadworku@gmail.com>
2025-12-13 15:10:10 -0600

    inference pipline for finetuned model

--- infer_finetuned.py
+++ infer_finetuned.py
@@ -1,66 +1,73 @@
-
-from transformers import GenerationConfig
+from transformers import Qwen2VLForConditionalGeneration, Qwen2VLProcessor
 from PIL import Image
 import torch
+from peft import PeftModel
 
 
-def infer_finetuned(model_path ="./Qwen-VL/Qwen-VL-Models/Qwen2-VL-Chat-Finetuned", imagepath= "./test-coconuts.png"):
-    model = Qwen2VLForConditionalGeneration.from_pretrained(
-        model_path,
-        device_map="auto",
-        dtype=torch.bfloat16,
-    )
+class InferencePipeline:
+    def __init__(self, 
+                base_model_path ="./Qwen-VL/Qwen-VL-Models/Qwen2-VL-Chat-Finetuned", 
+                adapter_path = "./Qwen-VL/Qwen-VL-Adapters/qwen2-vl-chat-finetuned-geolocrag-adapter"):
+        self.model = Qwen2VLForConditionalGeneration.from_pretrained(
+            base_model_path,
+            device_map="auto",
+            dtype=torch.bfloat16,
+        )
+        self.processor = Qwen2VLProcessor.from_pretrained(base_model_path)
 
-    processor = Qwen2VLProcessor.frompretrained(model_path)
+        self.model  = PeftModel.from_pretrained(self.model, adapter_path, device_map="auto", dtype=torch.bfloat16)
 
-    text=  processor.apply_chat_template()
+        self.model.eval()
 
-     # Format instruction
-    instruction = (
-        "You are expert in analyzing image and predicting the location from the scence identify common clues to identify the locaition and "
-        "return the contient, country, city , latitude longtiude of the given image output a json format as follow don't output extra result." \
-        "{result:{\"country\":\"\", \"city\":\"\", \"latitude\":, \"longitude\":}}"
-    )
-    image = Image.open(imagepath).convert("RGB")
-    # Create Qwen2-VL conversation format
-    messages = [
-        {
-            "role": "user",
-            "content": [
-                {"type": "image", "image": image},
-                {"type": "text", "text": instruction}
-            ]
-        }
-    ]
-        
-    # Apply chat template
-    text = processor.apply_chat_template(
-        messages, 
-        tokenize=False, 
-        add_generation_prompt=False
-    )
-        
-    message = {
-        "messages": text,
-        "images": [image],
-        "assistant_start_idx": len(processor.tokenizer.encode(
-            processor.apply_chat_template(
-                messages[:1], 
-                tokenize=False, 
-                add_generation_prompt=True
+    def infer_finetuned(self,imagepath, custom_instruction = None):
+        # Format instruction
+        if not custom_instruction:
+
+            instruction = (
+                "You are expert in analyzing image and predicting the location from the scence identify common clues to identify the locaition and "
+                "return the contient, country, city , latitude longtiude of the given image output a json format as follow don't output extra result." \
+                "{result:{\"country\":\"\", \"city\":\"\", \"latitude\":, \"longitude\":}}"
             )
-        ))
-    }
+        else:
+            instruction = custom_instruction
+        image = Image.open(imagepath).convert("RGB")
+        # Create Qwen2-VL conversation format
+        
+        template = [
+            {
+                "role": "user",
+                "content": [
+                    {"type": "image", "image": image},
+                    {"type": "text", "text": instruction},
+                ],
+            }
+        ]
+            
+        # Apply chat template
+        text = self.processor.apply_chat_template(
+            template, 
+            tokenizer =False,
+            add_generation_prompt=True
+        )
+        inputs = self.processor(
+            text=[text],
+            images=[image],
+            padding=True,
+            return_tensors="pt"
+        ).to(self.model.device)
 
-    generated_ids = model.generate(**messages, max_new_tokens=1024, generation_config=GenerationConfig(temperature=0.1, top_p=0.7, repetition_penalty=1.1))
+        # 4. Generate
+        with torch.no_grad():
+            print("Generating location deduction...")
+            generated_ids = self.model.generate(**inputs, max_new_tokens=512)
+            
+        # 5. Decode Output
+        generated_ids_trimmed = [
+            out_ids[len(in_ids):] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+        ]
+        output_text = self.processor.batch_decode(
+            generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
+        )[0]
 
-    generated_ids_trimmed = [out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)]
-
-    output_text = processor.batch_decode(
-        generated_ids_trimmed,
-        skip_special_tokens=True,
-        clean_up_tokenization_spaces=False
-    )
-
-    return output_text[0]
+        return output_text
 
